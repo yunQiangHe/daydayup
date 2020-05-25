ddf12bcc455b9a57e0e86094580effa025b84255
heyunqiang <heyunqiang99@163.com>
2020-05-25 12:07:08 +0800

    json-serve

--- 20200522/animal.js
+++ 20200522/animal.js
@@ -1,3 +1,18 @@
 class Animal {
-    constructor() {}
+    constructor(name) {
+        this.speed = 0;
+        this.name = name;
+    }
+
+    run(speed) {
+        this.speed = speed;
+        console.log(`${this.name} runs with speeds ${this.speed}`)
+    }
+
+    stop() {
+        this.speed = 0;
+        console.log(`${this.name} stands still.`);
+    }
 }
+
+let animal = new Animal("My animal");
